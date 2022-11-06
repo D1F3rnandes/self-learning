bcc833adf612688cef39f20a27d9acc921733b62
Diego Silva <diego.fernandes.co.en@gmail.com>
2022-11-06 19:29:36 -0300

    -

--- data-vizualization/Dashboard-analytics-with-Dash/App/dashboard_analytics.py
+++ data-vizualization/Dashboard-analytics-with-Dash/App/dashboard_analytics.py
@@ -1,6 +1,9 @@
-#Analytics Dashboard part of studies from Data Academy Science "Visualização de Dados e Design de Dashboards"
+'''
+Analytics Dashboard part of studies from 
+Data Academy Science "Visualização de Dados e Design de Dashboards" 
 
-#www.dataacademyscience.com.br
+www.dataacademyscience.com.br
+'''
 
 #Importing the packages
 import dash
@@ -17,48 +20,3 @@ from plotly.subplots import make_subplots
 from dash.dependencies import Input, Output, State
 import warnings
 warnings.filterwarnings("ignore")
-
-# Loading the data
-df = pd.read_csv('data/dataset.csv')
-
-# Calculating the gross profit margin
-df ['Margem_Lucro'] = np.multiply(np.divide(df['Lucro'], df['Venda']), 100).round(2)
-
-#Function for grouping
-def group_by(df, col):
-    
-    #Aggregation
-    grouped = df.groupby(by = col, as_index = False).agg({'Venda':'sum',
-                                                          'Lucro':'sum',
-                                                          'Quantidade':'sum',
-                                                          'Desconto':'mean',})
-    
-    #Calculating the profit margin
-    grouped['Margem_Lucro'] = np.multiply(np.divide(df['Lucro'], df['Venda']), 100).round(2)
-
-    return grouped
-
-#Formatting variables
-title_font = {'size':20, 'color':'black'}
-legend_font = {'size':20, 'color':'grey'}
-global_font = dict(family = 'Roboto')
-
-#
-#
-#
-#
-##### App Dash #####
-
-# Creating the App Dash
-dashboard_analytics = dash.Dash(__name__, 
-                external_stylesheets = [dbc.themes.YETI], 
-                suppress_callback_exceptions = True, 
-                meta_tags = [{'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0'}])
-
-server = dashboard_analytics.server
-#
-#
-#
-# App Executable
-if __name__=='__main__':
-    dashboard_analytics.run_server(debug = False, use_reloader = False)
