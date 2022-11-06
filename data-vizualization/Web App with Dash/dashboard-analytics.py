f59de71fd97a4825ea0f040567ce0c012ed6a53c
Diego Silva <diego.fernandes.co.en@gmail.com>
2022-11-06 15:58:25 -0300

    importing the packages

---
+++ data-vizualization/Web App with Dash/dashboard-analytics.py
@@ -0,0 +1,28 @@
+# Importing the packages
+import dash
+import plotly
+import locale
+import numpy as np
+import pandas as pd
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from dash.dependencies import Input, Output, State
+import warnings
+warnings.filterwarnings("ignore")
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
