2630306acd8d1eb8e46908aeae0236cbe01688ad
DIEGO FERNANDES SILVA <diego.fsilva45@senacsp.edu.br>
2023-02-27 20:31:32 -0300

    bugfixed

--- Programming_Languages/JAVA/Ex4/Ex4.java
+++ Programming_Languages/JAVA/Ex4/Ex4.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Ex4 {
     public static void main(String[] args) {
 
-        double salario1,novosalario;
+        double salario1,novoSalario;
         try (Scanner entrada = new Scanner(System.in)) {
             System.out.println("\nInsira o salario\n");
             salario1 = entrada.nextDouble();
         }
         
-        novosalario = (salario1*0.25)+salario1;
+        novoSalario = (salario1*0.25)+salario1;
         
-        System.out.println("\nNovo salario é de "+novosalario);
+        System.out.println("\nNovo salario é de "+novoSalario);
     }
     
 }
