a3d0f5f69f1a70d0e7bceb384e0f69c346587ddd
DIEGO FERNANDES SILVA <diego.fsilva45@senacsp.edu.br>
2023-02-27 20:29:26 -0300

    bugfixed

--- Programming_Languages/JAVA/Ex5/Ex5.java
+++ Programming_Languages/JAVA/Ex5/Ex5.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class Ex5 {
     public static void main(String[] args) {
 
-        double salario1,novosalario, percentual_decimal, aumentosalario;
+        double salario1,novosalario, percentualDecimal, aumentosalario;
         double percentual;
         try (Scanner entrada = new Scanner(System.in)) {
             System.out.println("\nInsira o salario\n");
             salario1 = entrada.nextDouble();
         }
         
-        percentual_decimal = 0.25;
-        percentual = percentual_decimal*100;
-        aumentosalario = salario1*percentual_decimal;
-        novosalario = (salario1*percentual_decimal)+salario1;
+        percentualDecimal = 0.25;
+        percentual = percentualDecimal*100;
+        aumentosalario = salario1*percentualDecimal;
+        novosalario = (salario1*percentualDecimal)+salario1;
         
         System.out.println("\nO seu salario teve um aumento de "+aumentosalario+" sendo "+percentual+"%. Novo salario é de "+novosalario);
     }
